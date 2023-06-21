ff21cc88d1b373f8b1f94de6e3a69fc459a0e8be
Puru raj <pururaj2000@gmail.com>
2023-06-21 08:30:23 +0530

    Added ecosystem

---
+++ ecosystem.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  apps : [{
+    script: 'npm run dev'
+  }],
+
+  deploy : {
+    production : {
+      key  : 'ubuntu.pem',
+      user : 'ubuntu',
+      host : '13.233.104.189',
+      ref  : 'origin/master',
+      repo : 'https://github.com/pururajatgithub/bookkart-frontend.git',
+      path : '/home/ubuntu/Desktop',
+      'pre-deploy-local': '',
+      'post-deploy' : 'source ~/.nvm/nvm && npm install && npm next build && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': '',
+      'ssh-options':'ForwardAgent=yes'
+    }
+  }
+};
