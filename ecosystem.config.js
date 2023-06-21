3dbc9e868d04e120a090e7084b0122e6e8bb0889
Puru raj <pururaj2000@gmail.com>
2023-06-22 01:18:25 +0530

    Final Commit

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,13 +6,13 @@ module.exports = {
   deploy : {
     production : {
       key  : 'ubuntu.pem',
-      user : 'root',
+      user : 'ubuntu',
       host : '43.204.149.220',
       ref  : 'origin/master',
       repo : 'https://github.com/pururajatgithub/bookkart-frontend.git',
       path : '/home/ubuntu/Desktop',
       'pre-deploy-local': '',
-      'post-deploy' : 'source /root/.nvm/nvm.sh && npm install && pm2 reload ecosystem.config.js --env production',
+    'post-deploy' : 'source /.nvm/nvm.sh && npm install && pm2 reload ecosystem.config.js --env production',
       'pre-setup': '',
       'ssh-options':'ForwardAgent=yes'
     }
